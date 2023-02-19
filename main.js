81b5eaacd89b1485abb011ca1fbb0f73ba6c8324
Davi_Lopes <sou_puro_osso@proton.me>
2023-02-19 07:11:29 -0300

    Added hjkl feature

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert("This is working!!!!")
+let keys = {
+  h() {
+	window.scrollBy(100, 0)
+  },
+  j() {
+	window.scrollBy(0, 100)
+  },
+  k() {
+	window.scrollBy(0, -100)
+  },
+  l() {
+	window.scrollBy(-100, 0)
+  },
+  g() {
+	window.scrollTo(0, 0)
+  }
+}
+
+document.addEventListener('keydown', (e) => {
+  keys[e.key]()
+})
