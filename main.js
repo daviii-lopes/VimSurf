68bb70b7bb036a7aaccdc5d47d1b4710721d6f32
Davi_Lopes <sou_puro_osso@proton.me>
2023-02-19 15:55:30 -0300

    Added function to "g" and "G"

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
+let config = {
+  scrollSize : 500,
+}
+
 let keys = {
   h() {
-	window.scrollBy(100, 0)
+	window.scrollBy(config.scrollSize, 0)
   },
   j() {
-	window.scrollBy(0, 100)
+	window.scrollBy(0, config.scrollSize)
   },
   k() {
-	window.scrollBy(0, -100)
+	window.scrollBy(0, -config.scrollSize)
   },
   l() {
-	window.scrollBy(-100, 0)
+	window.scrollBy(-config.scrollSize, 0)
   },
   g() {
 	window.scrollTo(0, 0)
+  },
+  G() {
+	window.scrollTo(0, document.documentElement.scrollHeight)
+  },
+  f() {
+	let link = document.querySelectorAll('a')
+	let letter = "oi"
+	for (let i = 0; i < link; i++) {
+	  link[i].after(letter)//`<div class="tooltip">${letter}</div>`)
+	}
   }
 }
 
 document.addEventListener('keydown', (e) => {
   keys[e.key]()
+  console.log(e)
+  if (e.shiftKey && e.key == "g") {
+	keys["G"]()
+  }
 })
