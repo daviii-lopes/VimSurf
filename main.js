0303b6d0fd6f7103cf52154ad41da51abbc57148
Davi_Lopes <sou_puro_osso@proton.me>
2023-02-21 09:57:49 -0300

    Version 1.0 complete

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let config = {
-  scrollSize : 500,
+  scrollSize : 300,
 }
-
 let keys = {
   h() {
 	window.scrollBy(config.scrollSize, 0)
@@ -21,19 +20,53 @@ let keys = {
   G() {
 	window.scrollTo(0, document.documentElement.scrollHeight)
   },
-  f() {
-	let link = document.querySelectorAll('a')
-	let letter = "oi"
-	for (let i = 0; i < link; i++) {
-	  link[i].after(letter)//`<div class="tooltip">${letter}</div>`)
+  showButtonsAndLinks() {
+	let array = []
+	for (let i = 0; i < document.querySelectorAll('a').length; i++) {	
+	  array.push(document.querySelectorAll('a')[i])
 	}
+	for (let i = 0; i < document.querySelectorAll('button').length; i++) {
+	  array.push(document.querySelectorAll('button')[i])
+	}
+	for (let i = 0; i < array.length; i++) {
+	  array[i].after(i)
+	  console.log(array)
+	}
+	return array
+  },
+  commandPrompt() {
+	let commands = {
+	  open(url = null) {
+		if (url = null) {
+		  alert("Something is wrong with your link")
+		} 
+		else {
+		  window.open("https://" + url)
+		}
+	  },
+	  click(index = 0) {
+		let array = []
+
+		array[index].click()
+	  }
+	}
+	let commandPrompt = window.prompt('Type something')
+	let promptArray = commandPrompt.split(' ')
+	commands[promptArray[0]](promptArray[1])
+		
   }
 }
 
+keys.showButtonsAndLinks()
 document.addEventListener('keydown', (e) => {
-  keys[e.key]()
+  if (keys[e.key]) {
+	keys[e.key]()
+  }
   console.log(e)
   if (e.shiftKey && e.key == "g") {
 	keys["G"]()
+  }
+  if (e.key == ":") {
+	keys.commandPrompt()
   }
 })
